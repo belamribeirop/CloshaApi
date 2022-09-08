c6aca2facead279ecf241164468fe9bb9281e3b5
Isabela Ribeiro <isabela_ribeiro@jabil.com>
2022-09-08 15:03:14 -0300

    feat: :sparkles: add user and item class

---
+++ Domain/Entities/Item.cs
@@ -0,0 +1,16 @@
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public StatusType Status { get; set; }
+    public NegociationType NegociationType { get; set; }
+    public CategoryType Category { get; set; }
+    public string Image { get; set; }
+    public IEnumerable<string> Tags { get; set; }
+
+}
+public enum CategoryType { }
+public enum NegociationType { }
+public enum StatusType { }
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+public class User
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Telephone { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Neighborhood { get; set; }
+    public string State { get; set; }
+    public string PostalCode { get; set; }
+    public string PhotoUrl { get; set; }
+}
